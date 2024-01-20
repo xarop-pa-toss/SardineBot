f739e0a589eae5e0e96e015d634ae7798e2bd75c
xarop-pa-toss <rpgdsantos@gmail.com>
2024-01-20 20:10:59 +0000

    Finish UrbanDictionary. No tests made. need to hide token in secrets.json but having trouble accessing it (need to study DI)

--- SardineBot/Commands/UrbanDictionary/UrbanDictionary.cs
+++ SardineBot/Commands/UrbanDictionary/UrbanDictionary.cs
@@ -2,12 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 using Discord.Commands;
-using Discord.Net.Rest;
 using RestSharp;
-using RestSharp.Authenticators;
 
 
 namespace SardineBot.Commands.UrbanDictionary
@@ -28,16 +27,37 @@ namespace SardineBot.Commands.UrbanDictionary
             await ReplyAsync(phrase);
         }
 
-        private async Task GetDefinitionFromUD(string phrase)
+        private async Task GetResponseFromUD(string phrase)
         {
             var options = new RestClientOptions("https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=wat");
 
             var client = new RestClient(options);
-            var request = new RestRequest();
+            var request = new RestRequest(phrase,Method.Get);
 
-            request.AddHeader("X-RapidAPI-Key", "SIGN-UP-FOR-KEY");
+            request.AddHeader("X-RapidAPI-Key", RapidAPI_UrbanDictionaryToken);
             request.AddHeader("X-RapidAPI-Host", "mashape-community-urban-dictionary.p.rapidapi.com");
-            IRestResponse response = client.Execute(request);
+            
+            var response = await client.GetAsync<UrbanDictionaryResponse>(request);
+
+            if (response?.List == null || !response.List.Any())
+            {
+                await ReplyAsync("Não foi encontrada definição para o termo procurado... aprende a escrever burro.");
+            }
+            else
+            {
+                await ReplyAsync($"_{phrase}?_");
+                await ReplyAsync(response.List[0].Definition);
+            }
         }
+    }
+
+    public class UrbanDictionaryResponse
+    {
+        public List<UrbanDictionaryItem>? List { get; set; }
+    }
+
+    public class UrbanDictionaryItem
+    {
+        public string? Definition { get; set; }
     }
 }
