636814086848f85a01722e937e782b646361a1b8
xarop-pa-toss <rpgdsantos@gmail.com>
2024-01-21 13:19:19 +0000

    Create Commands/GoogleSheets to handle commands to check each users club debt and subscription status

---
+++ SardineBot/Commands/GoogleSheets/GoogleSheets.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SardineBot.Commands.GoogleSheets
+{
+    internal class GoogleSheets : ModuleBase<SocketCommandContext>
+    {
+
+    }
+}
