d12ce7b0f87b1371e9117e166b8d5450d32a9fd4
xarop-pa-toss <rpgdsantos@gmail.com>
2024-03-09 21:34:19 +0000

    Change SlashCommand Choices to allow to check weather on different time intervals

--- SardineBot/Commands/Weather/Weather.cs
+++ SardineBot/Commands/Weather/Weather.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using Discord;
+using Discord.Interactions;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SardineBot.Commands.ForecaWeather
+namespace SardineBot.Commands.Weather
 {
-    internal class Weather
+    public class Weather : InteractionModuleBase
     {
+        private readonly IConfiguration _configuration;
+
+        public Weather(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [SlashCommand("tempo", "Busca o tempo para um certo local.")]
+        public async Task WeatherAsync(
+            [Choice("Hoje", "Portimão"), 
+            Choice("Amanhã", "Portimão"),
+            Choice("Semana", "Portimão"),
+            Choice("Agora", "Portimão")] string xoxa)
+        {
+            IDiscordInteraction command = Context.Interaction;
+
+            if (string.IsNullOrEmpty(xoxa))
+            {
+                await ReplyAsync("Utilização: /cidade <cidade a procurar>... burro");
+                return;
+            }
+            await GetResponseFromForecaWeather(xoxa);
+        }
+
+        public async Task GetResponseFromForecaWeather(string cidade)
+        {
+
+        }
     }
 }
