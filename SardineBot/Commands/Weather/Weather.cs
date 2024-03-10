05f5b14c52f308164fe504a4fba5d740c46f65e3
xarop-pa-toss <rpgdsantos@gmail.com>
2024-03-10 18:43:30 +0000

    Implement Forecast JSON handling. Need code for specific timeframe choices

--- SardineBot/Commands/Weather/Weather.cs
+++ SardineBot/Commands/Weather/Weather.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using RestSharp;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Text.Json.Nodes;
 
 namespace SardineBot.Commands.Weather
 {
@@ -24,70 +25,78 @@ namespace SardineBot.Commands.Weather
         public async Task WeatherAsync(string local, TempoTipos tempo)
         {
             IDiscordInteraction command = Context.Interaction;
-
             await GetResponseFromForecaWeather(local, tempo);
         }
 
         public async Task GetResponseFromForecaWeather(string local, TempoTipos tempo)
         {
             // Get Location ID from API. Used for all types of forecast calls
-            string? locationID = await GetLocationIDFromForecaWeather(local);
+            string localBaseURL = $"https://foreca-weather.p.rapidapi.com/location/search/{local}?lang=pt";
+            JObject? locationJson = await GetJsonObjectFromForecaWeather(localBaseURL);
 
+            string? locationID = locationJson["locations"]?[0]?["id"]?.ToString();
             if (string.IsNullOrEmpty(locationID))
             {
-                await ReplyAsync("Local inserido não foi encontrado.");
-                return;
+                await LogService.LogAsync(new LogMessage(LogSeverity.Error, "Weather.cs - GetJsonObjectFromForecaWeather", "Could not get JSON object from API."));
             }
 
-            // Time
-            RestClient client = null;
+
+
+            // Get Forecast data
+            string forecastBaseURL = "";
+
             switch (tempo)
             {
                 case TempoTipos.Agora:
-                    client = new RestClient($"https://foreca-weather.p.rapidapi.com/forecast/15minutely/{locationID}?alt=0&tempunit=C&windunit=KMH&periods=12&history=1&dataset=standard&lang=pt");
+                    //Nowcast
+                    forecastBaseURL = $"https://foreca-weather.p.rapidapi.com/forecast/15minutely/{locationID}?alt=0&tempunit=C&windunit=KMH&periods=12&history=1&dataset=standard&lang=pt";
                     break;
                 case TempoTipos.Hoje:
-                    client = new RestClient($"https://foreca-weather.p.rapidapi.com/forecast/hourly/{locationID}?alt=0&tempunit=C&windunit=KMH&periods=24&dataset=standard&lang=pt");
+                    //Hourly
+                    forecastBaseURL = $"https://foreca-weather.p.rapidapi.com/forecast/hourly/{locationID}?alt=0&tempunit=C&windunit=KMH&periods=24&dataset=standard&lang=pt";
                     break;
                 case TempoTipos.Amanhã:
-                    client = new RestClient($"https://foreca-weather.p.rapidapi.com/forecast/hourly/{locationID}?alt=0&tempunit=C&windunit=KMH&periods=48&dataset=standard&lang=pt");
+                    //Hourly
+                    forecastBaseURL = $"https://foreca-weather.p.rapidapi.com/forecast/hourly/{locationID}?alt=0&tempunit=C&windunit=KMH&periods=48&dataset=standard&lang=pt";
                     break;
                 case TempoTipos.Proximos7Dias:
-                    client = new RestClient($"https://foreca-weather.p.rapidapi.com/forecast/daily/{locationID}?alt=0&tempunit=C&windunit=KMH&periods=12&dataset=standard&lang=pt");
+                    //Daily
+                    forecastBaseURL = $"https://foreca-weather.p.rapidapi.com/forecast/daily/{locationID}?alt=0&tempunit=C&windunit=KMH&periods=12&dataset=standard&lang=pt";
                     break;
                 default:
                     break;
             }
 
-            if (client == null) 
-            {
-                await LogService.LogAsync(new LogMessage(LogSeverity.Error, "Weather.cs", "Error on Weather client API call."));
-                return;
-            }
+            JObject? forecastJson = await GetJsonObjectFromForecaWeather(localBaseURL);
 
-            RestRequest request = new RestRequest();
-            request.AddHeader("X-RapidAPI-Key", _token);
-            request.AddHeader("X-RapidAPI-Host", "foreca-weather.p.rapidapi.com");
-            var response = await client.GetAsync(request);
+            if (forecastJson["forecast"] is JArray forecastArray)
+            {
+                foreach (var item in forecastArray.Select((value, index) => new { Value = value, Index = index }))
+                {
+                    var thisItem = item.Value as JObject;
+
+                    string value = thisItem["element"].ToString();
+                    if (string.IsNullOrEmpty(value)) { }
+                }
+            }
+            string? timeOriginal = forecastJson["forecast"]?[0]?["time"]?.ToString();
         }
 
-        public async Task<string> GetLocationIDFromForecaWeather(string local)
+        public async Task<JObject?> GetJsonObjectFromForecaWeather(string baseURL)
         {
-            var client = new RestClient($"https://foreca-weather.p.rapidapi.com/location/search/{local}?lang=pt");
+            var client = new RestClient(baseURL);
             var request = new RestRequest();
             request.AddHeader("X-RapidAPI-Key", _token);
             request.AddHeader("X-RapidAPI-Host", "foreca-weather.p.rapidapi.com");
 
             var response = await client.GetAsync(request);
-            
             if (response.StatusCode == System.Net.HttpStatusCode.OK)
             {
-                var jsonObj = JObject.Parse(response.Content);
-                string? locationID = jsonObj["locations"]?[0]?["id"]?.ToString();
-                return locationID;
+                JObject? jsonObj = JObject.Parse(response.Content);
+                return jsonObj;
             }
 
-            return "";
+            return null;
         }
 
 
